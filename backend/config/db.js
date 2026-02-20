022fc36920054d28eaace815798161779bcfbf5b
Kalaiselvi-25 <kalaiselvi9182@gmail.com>
2026-02-21 00:45:28 +0530

    commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createPool({
   host: "localhost",
   user: "root",          // your MySQL username
-  password: "kalai",  // add your MySQL password
+  password: "",  // add your MySQL password
   database: "resource_booking"
 });
 
